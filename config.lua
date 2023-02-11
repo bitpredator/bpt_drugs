03b585846dec7894fd1837ca047e71a8baa9d14d
bitpredator <67551273+bitpredator@users.noreply.github.com>
2023-02-11 23:11:32 +0100

    Initial commit

---
+++ config.lua
@@ -0,0 +1,33 @@
+Config = {}
+Config.Locale      = 'it'
+
+Config.Delays = {
+	WeedProcessing = 1000 * 7
+}
+
+Config.DrugDealerItems = {
+	marijuana = 91
+}
+
+Config.GiveBlack = true -- give black money? if disabled it'll give regular cash.
+
+Config.CircleZones = {
+	WeedField = {coords = vector3(2220.72, 5582.52, 53.81), name = _U('blip_weedfield'), color = 25, sprite = 496, radius = 100.0},
+	WeedProcessing = {coords = vector3(2329.02, 2571.29, 46.68), name = _U('blip_weedprocessing'), color = 25, sprite = 496},
+
+	DrugDealer = {coords = vector3(-1172.02, -1571.98, 4.66), name = _U('blip_drugdealer'), color = 6, sprite = 378},
+}
+
+Config.Marker = {
+	Distance = 100.0,
+	Color = {r=60,g=230,b=60,a=255},
+	Size = vector3(1.5,1.5,1.0),
+	Type = 1,
+}
+
+-- min amount of Config.DrugDealerItems to sell
+-- max amount of Config.DrugDealerItems to sell
+Config.SellMenu = {
+	Min = 1,
+	Max = 50
+}
---
+++ locales/en.lua
@@ -0,0 +1,24 @@
+Locales ['en'] = {
+  -- weed
+  ['weed_pickupprompt'] = 'press [E] to harvest the Cannabis plant.',
+  ['weed_quitprocess'] = 'press [E] to stop Process Cannabis.',
+  ['weed_inventoryfull'] = 'you do not have any more inventory space for Cannabis.',
+  ['weed_processprompt'] = 'press [E] to start Process Cannabis.',
+  ['weed_processingstarted'] = 'processing Cannabis into Marijuana...',
+  ['weed_processingfull'] = 'processing ~r~canceled due to full inventory!',
+  ['weed_processingenough'] = 'you must have 3x Cannabis in order to process.',
+  ['weed_processed'] = 'you\'ve processed 3x Cannabis to 1x Marijuana',
+  ['weed_processingtoofar'] = 'the processing has been ~r~canceled due to you abandoning the area.',
+
+  -- drug dealer
+  ['dealer_prompt'] = 'press [E] to talk with the ~r~Drug Dealer.',
+  ['dealer_title'] = 'drug Dealer',
+  ['dealer_item'] = '$%s',
+  ['dealer_notenough'] = 'you don\'t have enough of that to sell!',
+  ['dealer_sold'] = 'you\'ve sold %sx %s for $%s',
+
+  -- blips
+  ['blip_weedfield'] = 'weed Field',
+  ['blip_weedprocessing'] = 'weed Processing',
+  ['blip_drugdealer'] = 'drug Dealer',
+}
